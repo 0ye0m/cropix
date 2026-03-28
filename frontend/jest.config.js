502344bffe87433e05afe45af7b0910a9643c7d5
Om Mandwade <146607354+0ye0m@users.noreply.github.com>
2026-03-28 10:33:23 +0530

    Update jest.config.js

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,10 +1,15 @@
-module.exports = {
-  testEnvironment: "jsdom",
-  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+const nextJest = require('next/jest')
+
+const createJestConfig = nextJest({
+  dir: './',
+})
+
+const customJestConfig = {
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  testEnvironment: 'jest-environment-jsdom',
   moduleNameMapper: {
-    "^@/(.*)$": "<rootDir>/$1",
+    '^@/(.*)$': '<rootDir>/$1',
   },
-  transform: {
-    "^.+\\.(ts|tsx)$": "ts-jest",
-  },
-};
+}
+
+module.exports = createJestConfig(customJestConfig)
